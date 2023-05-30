aaf1c05c73235c2da826841e756b70bc3aa9a88a
tanh11704 <tanh11704@gmail.com>
2023-05-30 16:10:31 +0700

    fix header

--- LOGIN/js/index.js
+++ LOGIN/js/index.js
@@ -2,6 +2,14 @@ const container = document.querySelector(".container");
 const btnSignIn = document.querySelector(".btnSign-in");
 const btnSignUp = document.querySelector(".btnSign-up");
 
+$(document).ready(function () {
+  $("#changeRegisterForm").click(function (e) {
+    e.preventDefault();
+    $(".sign_in").addClass("d-none");
+    $(selector).addClass(className);
+  });
+});
+
 btnSignIn.addEventListener("click", () => {
   container.classList.add("active");
 });
