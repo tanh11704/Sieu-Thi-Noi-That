e362dc91fa02115ee1c1f4f3aeab0d4b0c4317a0
tanh11704 <tanh11704@gmail.com>
2023-06-03 08:10:48 +0700

    fix desc_product

--- DES_PRODUCT/js/index.js
+++ DES_PRODUCT/js/index.js
@@ -3,4 +3,31 @@ $(document).ready(function () {
     e.preventDefault();
     $(this).find('input[type="radio"]').prop("checked", true);
   });
+  $("#addCart").click(function (e) {
+    e.preventDefault();
+    let name = $.trim(
+      $(this).closest(".choose__product").siblings(".product__title").text()
+    );
+    let price = parseFloat(
+      $(this)
+        .closest(".choose__product")
+        .find("#product-price")
+        .text()
+        .replaceAll(",", "")
+    );
+    let image_url =
+      "." + $(this).closest(".choose__product").find("#imgurl img").attr("src");
+
+    let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+    let product = {
+      name,
+      price,
+      image_url,
+    };
+
+    cartItems.push(product);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    alert("Thêm vào giỏ hàng thành công!");
+  });
 });
